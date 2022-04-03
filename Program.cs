28c288e36e092b610375ec89e61749d34bb7a32a
Taleh <t.mirzeyev@gmail.com>
2022-04-04 00:01:10 +0300

    creat function

--- Program.cs
+++ Program.cs
@@ -1,1 +1,11 @@
-﻿string[] array = {"hello","2","word","-)"};
+﻿string[] array = {"hello","--2","word","-)"};
+int j=0;
+for(int i=0;i<array.Length;i++){
+    for( j=0;j<array[i].Length;j++){
+        
+        }
+             if(j<=3){
+            Console.WriteLine(array[i]);
+    }
+
+}
