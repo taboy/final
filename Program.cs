cbdee3b0606e1c3be16a0c28096b60a76e086d99
a a <t.mirzeyev@mail.com>
2022-04-05 10:54:24 +0300

    add another void

--- Program.cs
+++ Program.cs
@@ -1,13 +1,34 @@
-﻿string[] array = {"hello","--2","word","(-)"};
-void finder(string[] finded){
-int j=0;
-for(int i=0;i<finded.Length;i++){
-    for( j=0;j<finded[i].Length;j++){
-        
-        }
-             if(j<=3){
-            Console.WriteLine(finded[i]);
-    }
+﻿string[] array = { "hello", "--2", "word", "(-)" };
+void finder(string[] finded)
+{
+    int j = 0;
+    for (int i = 0; i < finded.Length; i++)
+    {
+        for (j = 0; j < finded[i].Length; j++)
+        {
 
-}}
-finder(array);
+        }
+        if (j <= 3)
+        {
+            Console.WriteLine(finded[i]);
+        }
+
+    }
+}
+//finder(array);
+
+
+ void finder2(string[] finded)
+{
+
+    for (int i = 0; i < finded.Length; i++)
+    {
+
+        if (finded[i].Length <= 3)
+        {
+            Console.WriteLine(finded[i]);
+        }
+
+    }
+}
+finder2(array);
